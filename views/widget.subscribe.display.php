dc30ee56e63a33924825e2d1893242edce01b7bf
Nick Gundry <nick@nickgundry.com>
2013-09-19 22:58:31 +0200

    changed layout classes so bootstrap could be used

--- views/widget.subscribe.display.php
+++ views/widget.subscribe.display.php
@@ -20,7 +20,7 @@
     $show_last_name = (isset($instance['show_last_name']) && $instance['show_last_name']) ? true : false;
 
     // nonce
-    $nonce = wp_create_nonce("add_subscriber_nonce"); 
+    $nonce = wp_create_nonce("add_subscriber_nonce");
 
  ?>
  <div class="sailthru-signup-widget">
@@ -38,34 +38,36 @@
                 echo $before_title . esc_html(trim($title)) . $after_title;
             }
         ?>
-         <form method="post" action="#" id="sailthru-add-subscriber-form">
+         <form method="post" action="#" id="sailthru-add-subscriber-form" role="form">
             <div id="sailthru-add-subscriber-errors"></div>
+
             <?php if( $show_first_name ) { ?>
-            <div class="sailthru_form_input">
+            <div class="sailthru_form_input input-group">
                 <label for="sailthru_first_name">First Name:</label>
-                <input type="text" name="first_name" id="sailthru_first_name" value="" />
+                <input type="text" name="first_name" id="sailthru_first_name" value=""  class="form-control"/>
             </div>
             <?php } ?>
 
             <?php if( $show_last_name ) { ?>
-            <div class="sailthru_form_input">
+            <div class="sailthru_form_input input-group">
                 <label for="sailthru_last_name">Last Name:</label>
-                <input type="text" name="last_name" id="sailthru_last_name" value="" />
+                <input type="text" name="last_name" id="sailthru_last_name" value="" class="form-control"/>
             </div>
             <?php } ?>
 
-            <div class="sailthru_form_input">
-                <label for="sailthru_email">Email:</label>
-                <input type="text" name="email" id="sailthru_email" value="" />
+            <div class="input-group sailthru_form_input">
+                    <input type="text" name="email" id="sailthru_email" value="" class="form-control"/>
+                    <span class="input-group-btn">
+                        <input type="submit" value="Subscribe" class="btn btn-reverse"/>
+                    </span>
             </div>
-
-            <div class="sailthru_form_input">
-                <input type="hidden" name="sailthru_nonce" value="<?php echo $nonce; ?>" />
-                <input type="hidden" name="sailthru_email_list" value="<?php echo esc_attr($sailthru_list); ?>" />
-                <input type="hidden" name="action" value="add_subscriber" />
-                <input type="hidden" name="vars[source]" value="<?php bloginfo('url'); ?>" />
-                <input type="submit" value="Subscribe" />
-            </div>
+             <input type="hidden" name="sailthru_nonce" value="<?php echo $nonce; ?>" />
+            <input type="hidden" name="sailthru_email_list" value="<?php echo esc_attr($sailthru_list); ?>" />
+            <input type="hidden" name="action" value="add_subscriber" />
+            <input type="hidden" name="vars[source]" value="<?php bloginfo('url'); ?>" />
         </form>
+
+
+
     </div>
 </div>
