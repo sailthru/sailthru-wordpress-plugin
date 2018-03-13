1f7e0fbcf718f07054f0d8c0238df1039b925eb2
Rômulo Vitoi <romvitoi@gmail.com>
2018-03-13 13:24:36 -0300

    HVL-47 remove content description truncation

--- plugin.php
+++ plugin.php
@@ -248,7 +248,7 @@ function sailthru_save_post( $post_id, $post, $post_before ) {
 					if ( ! empty( $post->post_excerpt ) ) {
 						$data['description'] = $post->post_excerpt;
 					} else {
-						$data['description'] = wp_trim_words( $post->post_content, 250, '' );
+						$data['description'] = $post->post_content;
 					}
 					// image & thumbnail
 					if ( has_post_thumbnail( $post->ID ) ) {
