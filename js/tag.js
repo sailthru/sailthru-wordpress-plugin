3e91bdb78d33fe3bb3cb5890b67d421859bab543
Austin Brown <abrown@sailthru.com>
2020-04-07 11:33:04 -0400

    Add load event listener to only initialize JS when it gets loaded.

--- js/tag.js
+++ js/tag.js
@@ -1,17 +1,20 @@
 sailthru_init = function(){
-    if (tag.isCustom) {
-        jQuery(function($) {
-            Sailthru.init({
-                customerId: tag.options.customerId,
-                isCustom: true,
-                autoTrackPageview: tag.options.autoTrackPageview,
-                useStoredTags: tag.options.useStoredTags,
-                excludeContent: tag.options.excludeContent,
+    window.addEventListener("load", function() {
+        console.info("Sailthru onsite JS is loaded. Initializing Sailthru...");
+        if (tag.isCustom) {
+            jQuery(function($) {
+                Sailthru.init({
+                    customerId: tag.options.customerId,
+                    isCustom: true,
+                    autoTrackPageview: tag.options.autoTrackPageview,
+                    useStoredTags: tag.options.useStoredTags,
+                    excludeContent: tag.options.excludeContent,
+                });
             });
-        });
-    } else {
-        Sailthru.init({
-            customerId: tag.options.customerId
-        });
-    }
-}
+        } else {
+            Sailthru.init({
+                customerId: tag.options.customerId
+            });
+        }
+    });
+};
