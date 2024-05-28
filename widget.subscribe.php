c690cd1aa99e4f8aac92cc7e7a1df55d71168c21
PolepalliGayathri <145744302+PolepalliGayathri@users.noreply.github.com>
2024-05-28 16:13:19 +0530

    [INT-889] line formatting

--- widget.subscribe.php
+++ widget.subscribe.php
@@ -342,7 +342,7 @@ class Sailthru_Subscribe_Widget extends WP_Widget {
 
 				if ( ! empty( $customfields[ $field_key ]['sailthru_customfield_name'] ) ) {
 					$name_stripped = preg_replace( '/[^\da-z]/i', '_', $customfields[ $field_key ]['sailthru_customfield_name'] );
-                    $name_stripped = stripslashes($name_stripped);
+                                $name_stripped = stripslashes($name_stripped);
 					if ( ! empty( $_POST[ 'custom_' . $name_stripped ] ) ) {
 						// check to see if this is an array or a string
 						if ( is_array( $_POST[ 'custom_' . $name_stripped ] ) ) {
@@ -353,7 +353,7 @@ class Sailthru_Subscribe_Widget extends WP_Widget {
 							}
 						} else {
 							$var_name          = str_replace( 'custom_', '', $name_stripped );
-                            $vars[$var_name] = stripslashes(sanitize_text_field($_POST['custom_' . $name_stripped]));
+                                        $vars[$var_name] = stripslashes(sanitize_text_field($_POST['custom_' . $name_stripped]));
 						}
 					}
 				}
